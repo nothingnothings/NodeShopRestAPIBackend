e6745d70fab4a745e81a92f638d2ebe38305b6b3
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-09-07 15:40:01 -0300

    added timestamps to order objects

--- models/order.js
+++ models/order.js
@@ -2,28 +2,31 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const orderSchema = new Schema({
-  products: [
-    {
-      product: { type: Object, required: true, ref: 'Product' },
-      quantity: {
-        type: Number,
+const orderSchema = new Schema(
+  {
+    products: [
+      {
+        product: { type: Object, required: true, ref: 'Product' },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    user: {
+      userId: {
+        type: Schema.Types.ObjectId,
         required: true,
+        ref: 'User',
       },
     },
-  ],
-
-  totalPrice: {
-    type: Number,
-    required: true,
   },
-  user: {
-    userId: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: 'User',
-    },
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Order', orderSchema);
