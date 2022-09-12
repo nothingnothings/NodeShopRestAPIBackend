1cdc3a4c58c2526b6838b5524800f140a0837d34
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-09-12 15:59:06 -0300

    minor adjustments

--- routes/admin.js
+++ routes/admin.js
@@ -1,40 +1,19 @@
 const express = require('express');
-
 const router = express.Router();
 
 const isAuth = require('../middleware/is-auth');
-
 const shopController = require('../controllers/shop');
-
 const adminController = require('../controllers/admin');
 
 router.get('/admin-shop', isAuth, shopController.getProducts);
 
-router.get(
-  '/orders',
-  isAuth,
-  adminController.getOrders
-);
+router.get('/orders', isAuth, adminController.getOrders);
 
+router.get('/orders/:orderId', isAuth, shopController.getFatura);
 
-router.get(
-  '/orders/:orderId',
-  isAuth,
-  shopController.getFatura
-);
+router.get('/cart', isAuth, adminController.getCart);
 
-
-router.get(
-  '/cart',
-  isAuth,
-  adminController.getCart
-);
-
-router.post(
-  '/cart',
-  isAuth,
-  adminController.postToCart
-);
+router.post('/cart', isAuth, adminController.postToCart);
 
 router.delete('/cart', isAuth, adminController.deleteCartItem);
 
